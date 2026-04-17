cb67b65963b8d6cf663da5c2e5a9ec242ca9cfe7
L1nf3ng <nuaa_llf@126.com>
2026-04-18 00:52:01 +0800

    补充项目依赖

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from deepagents import create_deep_agent
+from deepagents.backends import FilesystemBackend, LocalShellBackend
+from deepagents.backends.local_shell import LocalShellBackend
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 
@@ -7,11 +9,14 @@ load_dotenv()
 model = init_chat_model("anthropic:MiniMax-M2.5")
 agent = create_deep_agent(
     model=model,
+    backend=FilesystemBackend(root_dir=".", virtual_mode=True)
 )
 
 
 if __name__ == "__main__":
-    result = agent.invoke({"messages": [{"role": "user", "content": "What is langgraph?"}]})
+    result = agent.invoke({"messages": [{"role": "user", "content": "访问这个页面https://platform.minimaxi.com/docs/api-reference/text-anthropic-api 并做总结，结果写入/results目录"}]})
     
     # Print the agent's response
-    print(result["messages"][-1].content)
+    for message in result["messages"][-1].content:
+        if message["type"]=='text':
+            print(message['text'])
